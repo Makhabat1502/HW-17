b3feac155c4c0f48f39588c5dd17003aa18a08e7
Makhabat Berdibekova <makhabat.berdibekova.kk@gmail.com>
2022-03-16 16:24:31 +0600

    complete task1

---
+++ destructuring_array/task1/js.js
@@ -0,0 +1,8 @@
+let arr = ['Иван', 'Иванов', 'отдел разработки', 'программист', 2000];
+let [firstName, surname, department, position, salary] = arr;
+
+console.log(firstName);
+console.log(surname);
+console.log(department);
+console.log(position);
+console.log(salary);
